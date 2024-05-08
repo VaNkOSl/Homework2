f9f81c91621bea1ab82092b2f7544129ecb92a23
VaNkOSl <158475958+VaNkOSl@users.noreply.github.com>
2024-05-08 11:53:04 +0300

    Add files via upload

---
+++ TrainingComponentBase.cs
@@ -0,0 +1,12 @@
+﻿using Homework2.Third_Task.Contacts;
+
+namespace Homework2.Third_Task
+{
+    public abstract class TrainingComponentBase : ITrainingComponent
+    {
+        public string Description { get; set; }
+
+
+        public abstract object Clone();
+    }
+}
